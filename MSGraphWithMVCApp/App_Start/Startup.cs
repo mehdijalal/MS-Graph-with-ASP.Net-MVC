71833941065d0cf4c5aa887ccae163f264e039f7
Mehdi Jalal <mehdi.jalal@live.com>
2019-08-25 02:28:58 -0400

    Initial commit

---
+++ MSGraphWithMVCApp/App_Start/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+[assembly: OwinStartup(typeof(MSGraphWithMVCApp.App_Start.Startup))]
+
+namespace MSGraphWithMVCApp.App_Start
+{
+    public class Startup
+    {
+        private static string Client_ID = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string AzureADInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string YourOffice365Tenent = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string LogoutRedirect = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        string authority = String.Format(CultureInfo.InvariantCulture, AzureADInstance, YourOffice365Tenent);
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+            new OpenIdConnectAuthenticationOptions
+            {
+                ClientId = Client_ID,
+                Authority = authority,
+                PostLogoutRedirectUri = LogoutRedirect,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthenticationFailed = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect("/ Error / message =" +context.Exception.Message);
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ MSGraphWithMVCApp/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+using System.Web.Mvc;
+using Microsoft.Owin.Host.SystemWeb;
+using System.Web;
+
+namespace MSGraphWithMVCApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties
+                {
+                    RedirectUri = "/"
+                    }, OpenIdConnectAuthenticationDefaults.AuthenticationType
+                );
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+            OpenIdConnectAuthenticationDefaults.AuthenticationType,
+            CookieAuthenticationDefaults.AuthenticationType
+            );
+        }
+    }
+
+}
